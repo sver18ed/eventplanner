6d852a0ca64ba16518d2ea10fc53e3deabe27e31
AndreasMalmstrom <andreas.malmstroem@gmail.com>
2020-02-14 13:40:10 +0100

    Worked on the calendar

--- web-application/src/presentation-layer/routers/various-router.js
+++ web-application/src/presentation-layer/routers/various-router.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get("/", function(request, response){
+router.get("/", function(request, response){	
 	response.render("home.hbs")
 })
 
